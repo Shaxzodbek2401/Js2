94e4d2aadd60be62dac985fed300f65c9a1fe37b
Js2 <Js2@gmail.com>
2021-04-29 07:36:23 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var sekundy = +prompt('Введите число секунд:  ')
+alert('Уже в консоле)))')
+console.log(sekundy + ' Секунд')
+var minut = sekundy / 60;
+console.log("Процесс.")
+console.log("Процесс..")
+console.log("Процесс...")
+console.log(minut + ' минут')
+var hour = minut / 60;
+console.log(hour + ' часов')
+var day = hour / 24;
+console.log(day + ' сутки')
